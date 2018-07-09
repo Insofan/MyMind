e7b0229c8e0b0bb61bf2232a153bb65c194ce856
Insofan <282132315@qq.com>
2018-07-09 14:18:18 +0800

    init effective oc

---
+++ Effective Objective-C 2.0/Effective Objective-C 2.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Effective Objective-C 2.0
+//
+//  Created by Insomnia on 2018/7/9.
+//  Copyright © 2018年 Insomnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Effective Objective-C 2.0/Effective Objective-C 2.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Effective Objective-C 2.0
+//
+//  Created by Insomnia on 2018/7/9.
+//  Copyright © 2018年 Insomnia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
