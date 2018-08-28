9eb1cf47ce0d0938d0aa528a3bfb91ba7e02dd90
Insofan <282132315@qq.com>
2018-08-28 10:15:15 +0800

    TCP IP ch6 udp EchoCli EchoSer

--- TCP-IP-Programming/ch6/ch6EchoCli.cpp
+++ TCP-IP-Programming/ch6/ch6EchoCli.cpp
@@ -3,9 +3,58 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
 
+
+#define  BUF_SIZE 30
+
+void errorHandling(char *message);
+
+using namespace std;
 
 int main() {
+    int sock;
+    char message[BUF_SIZE];
+    int strLen;
+    socklen_t addrSize;
+    
+    struct sockaddr_in servAddr, fromAddr;
+    
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        errorHandling("sock err");
+    }
+    
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servAddr.sin_port = htons(6666);
+    
+    while (1) {
+        fputs("input message(Q to quit): ", stdout);
+        fgets(message, BUF_SIZE, stdin);
+        if (!strcmp(message, "q\n") || !strcmp(message, "Q\n")) {
+            break;
+        }
+        
+        sendto(sock, message, strlen(message), 0, (struct sockaddr*) &servAddr, sizeof(servAddr));
+        addrSize = sizeof(fromAddr);
+        
+        strLen = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr *)&fromAddr, &addrSize);
+        message[strLen] = 0;
+        cout << "Message from server : " << message << endl;
+    }
+    close(sock);
 
     return 0;
+}
+void errorHandling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
--- TCP-IP-Programming/ch6/ch6EchoSer.cpp
+++ TCP-IP-Programming/ch6/ch6EchoSer.cpp
@@ -3,9 +3,54 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
 
+
+#define  BUF_SIZE 30
+
+void errorHandling(char *message);
+
+using namespace std;
 
 int main() {
+    int                servSock;
+    char               message[BUF_SIZE];
+    int                strLen;
+    socklen_t          clientAddrSize;
+    struct sockaddr_in servAddr, clientAddr;
 
+    servSock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (servSock < 0) {
+        errorHandling("udp sock err");
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family      = AF_INET;
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servAddr.sin_port = ntohs(6666);
+
+    int bindFlag = bind(servSock, (struct sockaddr*)&servAddr, sizeof(servAddr));
+    if (bindFlag < 0) {
+        errorHandling("bind err");
+    }
+
+    while (1) {
+       clientAddrSize = sizeof(clientAddr);
+       strLen = recvfrom(servSock, message, BUF_SIZE, 0, (struct sockaddr *) &clientAddr, &clientAddrSize);
+       cout << "Message from client: " << message << endl;
+       sendto(servSock, message, strLen, 0, (struct sockaddr *)&clientAddr, clientAddrSize);
+    }
+    close(servSock);
     return 0;
+}
+
+void errorHandling(char *message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
