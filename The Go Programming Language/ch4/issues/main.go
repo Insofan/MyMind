2f459730c730a894e031a31db0f245d3685495bd
Insomnia <282132315@qq.com>
2018-09-07 00:23:51 +0800

    sync

--- The Go Programming Language/ch4/issues/main.go
+++ The Go Programming Language/ch4/issues/main.go
@@ -6,4 +6,24 @@
  * Time: 上午12:29
  */
 
-package issues
+package main
+
+import (
+	"../github"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	//输入  ./main repo:golang/go is:open json decoder
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %9.9s %55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
