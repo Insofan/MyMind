a8ef79099babb5df2b6f15372d02c16929624d9b
Insomnia <282132315@qq.com>
2018-09-12 00:22:55 +0800

    gwp init template css

---
+++ GoWebProgramming/ch2/main.go
@@ -0,0 +1,28 @@
+/**
+ * Created with Goland.
+ * Description:
+ * User: Insomnia
+ * Date: 2018-09-12
+ * Time: 上午12:11
+ */
+
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", index)
+
+	server := &http.Server(
+	/*
+		Addr: "0.0.0.0:8080",
+		Handler: mux,
+	*/
+
+	)
+	server.ListenAndServe()
+}
