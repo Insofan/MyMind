92fc15b360946369569140bf86be4e5b8e9c45fe
Insomnia <282132315@qq.com>
2018-09-13 00:05:56 +0800

    https

--- GoWebProgramming/ch3/https/main.go
+++ GoWebProgramming/ch3/https/main.go
@@ -1,9 +1,38 @@
 /**
  * Created with Goland.
- * Description: 
+ * Description:
  * User: Insomnia
  * Date: 2018-09-12
  * Time: 下午10:51
  */
 
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/http2"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello https server")
+}
+
+func main() {
+
+	handler := MyHandler{}
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: &handler,
+	}
+
+	http2.ConfigureServer(&server, &http2.Server{})
+	err := server.ListenAndServeTLS("cert.pem", "key.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
