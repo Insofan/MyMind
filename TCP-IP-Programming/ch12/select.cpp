ae775d17d475491b1622fbdcc0965d9bdc32eed8
Insofan <282132315@qq.com>
2018-09-13 11:20:13 +0800

    rebase

---
+++ TCP-IP-Programming/ch12/select.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Insomnia on 2018/9/12.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+
+
+#define BUF_SZIE 30
+
+int main() {
+
+    fd_set reads, temps;
+    int result, strLen;
+    char buf[BUF_SZIE];
+    struct timeval
+
+    return 0;
+}
