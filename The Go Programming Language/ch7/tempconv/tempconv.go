5ef68142ded841436d5ed1a0e98320aa951758da
Insomnia <282132315@qq.com>
2018-09-14 00:03:44 +0800

    sync

--- The Go Programming Language/ch7/tempconv/tempconv.go
+++ The Go Programming Language/ch7/tempconv/tempconv.go
@@ -7,3 +7,33 @@
  */
 
 package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+func CToF(c Celsius) Fahrenheit {
+
+}
+
+type celsiusFlag struct {
+	Celsius
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+
+	switch unit {
+	case "C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F":
+		f.Celsius = FToC()
+		return nil
+	}
+
+	return fmt.Errorf("invalid temprature %q", s)
+}
