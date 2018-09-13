7a4c56e1860d0157dc06ef942d4efbe0d5d02911
Insofan <282132315@qq.com>
2018-09-13 11:36:10 +0800

    tcp ip ch12 select

--- TCP-IP-Programming/ch12/select.cpp
+++ TCP-IP-Programming/ch12/select.cpp
@@ -15,7 +15,34 @@ int main() {
     fd_set reads, temps;
     int result, strLen;
     char buf[BUF_SZIE];
-    struct timeval
+    struct timeval timeout;
+    //初始化fd set 变量
+    FD_ZERO(&reads);
+    FD_SET(0, &reads);
+
+    while (1) {
+        //记住初始值
+        temps = reads;
+
+        timeout.tv_sec = 5;
+        timeout.tv_usec = 0;
+        //有数据输出大于0, 无等于0
+        result = select(1, &temps, 0, 0, &timeout);
+
+        if (result == -1) {
+            puts("select() err");
+            break;
+        } else if (result == 0) {
+            puts("time out");
+        } else {
+            //验证标准输出
+            if (FD_ISSET(0, &temps)) {
+                strLen = read(0, buf, BUF_SZIE);
+                buf[strLen] = 0;
+                printf("message from console: %s", buf);
+            }
+        }
+    }
 
     return 0;
 }
