1e0d03d5ddd4879c5af79703ca6760068fb7e0a7
Insofan <282132315@qq.com>
2018-09-14 09:07:03 +0800

    sync

---
+++ GoInAction/ch2/main.go
@@ -0,0 +1,21 @@
+//
+//  main.go
+//  GoInAction 
+//
+//  Created by Inso on 2018/9/13.
+//  Copyright © 2018 Inso. All rights reserved.
+//
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetOutput(os.Stdout)
+}
+
+func main() {
+
+}
---
+++ GoInAction/ch2/search/search.go
@@ -0,0 +1,11 @@
+//
+//  search.go
+//  GoInAction 
+//
+//  Created by Inso on 2018/9/13.
+//  Copyright © 2018 Inso. All rights reserved.
+//
+package search
+
+
+var matchers = make(map[string]Matcher)
