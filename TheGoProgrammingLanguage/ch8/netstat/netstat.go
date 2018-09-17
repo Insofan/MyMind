90d5d7356a823362e564a6bfaef65b43c5a1c228
Insomnia <282132315@qq.com>
2018-09-18 00:52:26 +0800

    gopl ch8 clock2

---
+++ TheGoProgrammingLanguage/ch8/netstat/netstat.go
@@ -0,0 +1,31 @@
+/**
+ * Created with Goland.
+ * Description:
+ * User: Insomnia
+ * Date: 2018-09-18
+ * Time: 上午12:32
+ */
+
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
