9042836efb93790eca3e80d6d9602c734ffe2b9a
Insofan <282132315@qq.com>
2018-09-25 17:02:56 +0800

    sync

---
+++ Quizz/ObjCQuizz/ObjCQuizz/KVO/KVOImplement/KVOImp/NSObject+hx_KVO.h
@@ -0,0 +1,10 @@
+//
+// Created by Insomnia on 2018/9/25.
+// Copyright (c) 2018 Insomnia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (hx_KVO)
+- (void)hx_addObserver(id):observer key:(NSString *)key callback:
+@end
---
+++ Quizz/ObjCQuizz/ObjCQuizz/KVO/KVOImplement/KVOImp/NSObject+hx_KVO.m
@@ -0,0 +1,10 @@
+//
+// Created by Insomnia on 2018/9/25.
+// Copyright (c) 2018 Insomnia. All rights reserved.
+//
+
+#import "NSObject+hx_KVO.h"
+
+
+@implementation NSObject (hx_KVO)
+@end
