c5c4e8e912b9823ef73ac8afe84dcc6f1db6494e
Insomnia <282132315@qq.com>
2018-10-01 23:59:44 +0800

    gwp ch4 prepare cookie

--- GoWebProgramming/ch4/writer/server.go
+++ GoWebProgramming/ch4/writer/server.go
@@ -9,14 +9,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Post struct {
+	User    string
+	Threads []string
+}
+
 func writeExample(w http.ResponseWriter, r *http.Request) {
 	str := `<html>
 <head><title>Go Web Programming</title></head>
-<body><h1>Hello world</h1></body>
+<body><h1>Hello World</h1></body>
 </html>`
 	//	w.Write([]byte(str))
 	fmt.Fprintln(w, str)
@@ -51,6 +57,26 @@ func headerExample(w http.ResponseWriter, r *http.Request) {
 	*/
 }
 
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	post := &Post{
+		User:    "Insomnia",
+		Threads: []string{"first", "second", "third"},
+	}
+
+	json, _ := json.Marshal(post)
+	w.Write(json)
+	/*
+			HTTP/1.1 200 OK
+		Content-Type: application/json
+		Date: Mon, 01 Oct 2018 15:57:20 GMT
+		Content-Length: 56
+
+		{"User":"Insomnia","Threads":["first","second","third"]}
+	*/
+
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
@@ -60,5 +86,6 @@ func main() {
 	// curl -i 127.0.0.1:8080/writeheader
 	http.HandleFunc("/writeheader", writeHeaderExample)
 	http.HandleFunc("/redirect", headerExample)
+	http.HandleFunc("/json", jsonExample)
 	server.ListenAndServe()
 }
