c4e95b4bea5105b920350741ec5658328f163e06
Insomnia <282132315@qq.com>
2018-10-02 12:49:14 +0800

    gwp ch5 dot

---
+++ GoWebProgramming/ch5/iterator/server.go
@@ -0,0 +1,29 @@
+/**
+ * Created with Goland.
+ * Description:
+ * User: Insomnia
+ * Date: 2018-10-02
+ * Time: 下午12:35
+ */
+
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("ch5/iterator/tmpl.html")
+	daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+	t.Execute(w, daysOfWeek)
+
+}
+
+func main() {
+	server := http.Server{
+		Addr: ":8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
---
+++ GoWebProgramming/ch5/setDot/server.go
@@ -0,0 +1,26 @@
+/**
+ * Created with Goland.
+ * Description:
+ * User: Insomnia
+ * Date: 2018-10-02
+ * Time: 下午12:45
+ */
+
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("ch5/setDot/tmpl.html")
+	t.Execute(w, "hello")
+}
+func main() {
+	server := http.Server{
+		Addr: ":8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
