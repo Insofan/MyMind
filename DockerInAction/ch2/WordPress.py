26be1de4fb8960eb341f62f7d473d09cf3534b60
Insomnia <282132315@qq.com>
2018-11-26 01:05:08 +0800

    sync

--- DockerInAction/ch2/WordPress.py
+++ DockerInAction/ch2/WordPress.py
@@ -9,5 +9,15 @@ import utils.SudoCmd
 
 if __name__ == '__main__':
     cmd = utils.SudoCmd.sudoCmd()
-    db = 'docker run -d --name wpdb -e MYSQL_ROOT_PASSWORD=ch2demo mysql:5'
-    cmd.run(db)
+    db = 'docker run -d --name db2 -p 13306:3306 -p 13060:33060 -e MYSQL_ROOT_PASSWORD=ch2demo mysql:5'
+    # 下面会报错
+    # wp = 'docker run -d --name wp2 --link wpdb:mysql -p 80 --read-only wordpress:4'
+    # wp2 = 'docker run -d --name wp3 --link wpdb:mysql -p 80 -v /run/lock/apache2/ -v /run/apache2/ --read-only ' \
+    #       'wordpress:4'
+    #  这里-e WORDPRESS_DB_HOST=用来指定端口
+    #  用ifconfig 查看docker0 的虚拟地址, 然后 连接这个地址 expose 映射的3306地址
+    wp3 = 'docker run -d --name wp4 -e WORDPRESS_DB_HOST=172.17.0.1:13306 --link wpdb:mysql -p 80 -v ' \
+          '/run/lock/apache2/ -v /run/apache2/ -v /tmp/ ' \
+          '--read-only ' \
+          'wordpress:4'
+    cmd.run(wp3)
