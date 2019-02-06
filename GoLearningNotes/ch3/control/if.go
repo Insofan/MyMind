9401f6e21673fee420a1b10d6bca4a9d763b00d6
Insofan <282132315@qq.com>
2019-02-07 01:07:41 +0800

    sync

---
+++ GoLearningNotes/ch3/control/if.go
@@ -0,0 +1,18 @@
+//
+//  if.go
+//  GoLearningNotes 
+//
+//  Created by Inso on 2019/2/7.
+//  Copyright © 2019 Inso. All rights reserved.
+//
+package main
+
+func main() {
+	x := 10
+	if a, b := x +1, x+ 10; a < b {
+		println(a)
+	} else {
+		println(b)
+	}
+
+}
